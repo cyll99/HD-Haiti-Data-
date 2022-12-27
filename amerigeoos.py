8ac06adffbe5685cbea757d424a7431a4c93b47a
cyll99 <christyanlove@gmail.com>
2022-12-27 10:00:21 -0800

    first commit

---
+++ amerigeoos.py
@@ -0,0 +1,22 @@
+from bs4 import BeautifulSoup as soup  # HTML data structure
+from urllib.request import urlopen as uReq  # Web client
+import requests
+
+page_url = "https://data.amerigeoss.org/gl/group/amerigeoss?q=haiti"
+# opens the connection and downloads html page from url
+try:
+    uClient = requests.get(page_url).text
+except:
+    print("Connection failed")
+
+# parses html into a soup data structure to traverse html
+# as if it were a json data type.
+page_soup = soup(uClient, "html.parser")
+
+
+# finds each product from the store page
+containers = page_soup.findAll("li", {"class": "dataset-item"})
+
+for container in containers:
+    print(container.a.text)
+    
