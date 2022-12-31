b99a1f7585b9bdd3531d74c5a003eadaa0e26cef
cyll99 <christyanlove@gmail.com>
2022-12-30 21:00:25 -0800

    flask added

--- ocha.py
+++ ocha.py
@@ -2,21 +2,31 @@ from bs4 import BeautifulSoup as soup  # HTML data structure
 from urllib.request import urlopen as uReq  # Web client
 import requests
 
-page_url = "https://data.humdata.org/dataset?q=haiti"
-# opens the connection and downloads html page from url
-try:
-    uClient = requests.get(page_url).text
-except:
-    print("Connection failed")
 
-# parses html into a soup data structure to traverse html
-# as if it were a json data type.
-page_soup = soup(uClient, "html.parser")
+class Ocha:
+    def __init__(self):
+        
+        page_url = "https://data.humdata.org/dataset?q=haiti"
+        # opens the connection and downloads html page from url
+        try:
+            uClient = requests.get(page_url).text
+        except:
+            print("Connection failed")
 
+        # parses html into a soup data structure to traverse html
+        # as if it were a json data type.
+        page_soup = soup(uClient, "html.parser")
 
-# finds each product from the store page
-containers = page_soup.findAll("li", {"class": "list-items dataset-item"})
+        self.titles, self.descriptions, self.details = list(), list(), list()
 
-for container in containers:
-    print(container.a.text)
-    
+        # finds each product from the store page
+        containers = page_soup.findAll("li", {"class": "list-items dataset-item"})
+
+        for container in containers:
+            self.titles.append(container.a.text)
+            link = "https://data.humdata.org"+container.a["href"]
+            self.details.append(link)
+            print(link)            
+            info = container.findAll("div", {"class" : "dataset-dates"})
+            self.descriptions.append(info[0].text.strip().replace('\n', '').replace("\t", ""))
+        
