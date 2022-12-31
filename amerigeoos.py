83955b4660e6f796619f594c2c7086298d499908
cyll99 <christyanlove@gmail.com>
2022-12-30 21:01:01 -0800

    flask added

--- amerigeoos.py
+++ amerigeoos.py
@@ -2,21 +2,28 @@ from bs4 import BeautifulSoup as soup  # HTML data structure
 from urllib.request import urlopen as uReq  # Web client
 import requests
 
-page_url = "https://data.amerigeoss.org/gl/group/amerigeoss?q=haiti"
-# opens the connection and downloads html page from url
-try:
-    uClient = requests.get(page_url).text
-except:
-    print("Connection failed")
+class Amerigeoos:
+    def __init__(self):
+        
+        page_url = "https://data.amerigeoss.org/gl/group/amerigeoss?q=haiti"
+        # opens the connection and downloads html page from url
+        try:
+            uClient = requests.get(page_url).text
+        except:
+            print("Connection failed")
 
-# parses html into a soup data structure to traverse html
-# as if it were a json data type.
-page_soup = soup(uClient, "html.parser")
+        # parses html into a soup data structure to traverse html
+        # as if it were a json data type.
+        page_soup = soup(uClient, "html.parser")
 
+        self.titles, self.descriptions, self.details = list(), list(), list()
 
-# finds each product from the store page
-containers = page_soup.findAll("li", {"class": "dataset-item"})
+        # finds each product from the store page
+        containers = page_soup.findAll("li", {"class": "dataset-item"})
 
-for container in containers:
-    print(container.a.text)
-    
+        for container in containers:
+            link = "https://data.amerigeoss.org"+container.a["href"]
+            self.details.append(link)
+            self.titles.append(container.a.text)
+            self.descriptions.append(container.div.div.text)
+        
---
+++ main.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, request
+from amerigeoos import Amerigeoos
+
+from ocha import Ocha
+
+ocha = Ocha()
+amerigeoos = Amerigeoos()
+
+ocha_data = zip(ocha.titles, ocha.descriptions, ocha.details)
+amerigeoos_data = zip(amerigeoos.titles, amerigeoos.descriptions, amerigeoos.details)
+
+app = Flask(__name__)
+
+@app.route('/',  methods =["GET", "POST"])
+def result():
+
+    # if request.method == "POST":
+    #     # getting input_key in HTML form
+    #     input_key = request.form.get("search")
+    #     file_name = f"{path}/{input_key}.csv"
+    #     notice = f'You will also find the result in {path}'
+    #     download_data(input_key, file_name)
+    #     with open(f"{path}/{input_key}_by_prices.csv", encoding="UTF-8") as csv_file:
+    #         reader = csv.DictReader(csv_file)
+    #         return render_template('index.html', result = reader, notice=notice)
+    return render_template("index.html", ocha = ocha_data, amerigeoos = amerigeoos_data)
+
+if __name__ == '__main__':
+    app.run(debug = True)
