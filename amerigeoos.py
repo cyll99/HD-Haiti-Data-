8139137b294a5a12d2477f4b2d2cdba55e6d1c73
cyll99 <christyanlove@gmail.com>
2022-12-30 21:56:44 -0800

    scraping multiple pages

--- amerigeoos.py
+++ amerigeoos.py
@@ -5,25 +5,31 @@ import requests
 class Amerigeoos:
     def __init__(self):
         
-        page_url = "https://data.amerigeoss.org/gl/group/amerigeoss?q=haiti"
-        # opens the connection and downloads html page from url
-        try:
-            uClient = requests.get(page_url).text
-        except:
-            print("Connection failed")
-
-        # parses html into a soup data structure to traverse html
-        # as if it were a json data type.
-        page_soup = soup(uClient, "html.parser")
+        page_url = "https://data.amerigeoss.org/gl/group/amerigeoss?q=haiti&page="
 
         self.titles, self.descriptions, self.details = list(), list(), list()
 
-        # finds each product from the store page
-        containers = page_soup.findAll("li", {"class": "dataset-item"})
 
-        for container in containers:
-            link = "https://data.amerigeoss.org"+container.a["href"]
-            self.details.append(link)
-            self.titles.append(container.a.text)
-            self.descriptions.append(container.div.div.text)
+        for i in range(1, 5):
+
+            page_url = page_url + str(i)   
+            # opens the connection and downloads html page from url
+            try:
+                uClient = requests.get(page_url).text
+            except:
+                print("Connection failed")
+
+            # parses html into a soup data structure to traverse html
+            # as if it were a json data type.
+            page_soup = soup(uClient, "html.parser")
+
+
+            # finds each product from the store page
+            containers = page_soup.findAll("li", {"class": "dataset-item"})
+
+            for container in containers:
+                link = "https://data.amerigeoss.org"+container.a["href"]
+                self.details.append(link)
+                self.titles.append(container.a.text)
+                self.descriptions.append(container.div.div.text)
         
