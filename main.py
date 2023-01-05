e94329353d03956bf34059bc257d886b0ee6c5da
cyll99 <christyanlove@gmail.com>
2023-01-04 16:32:23 -0800

    search done

--- main.py
+++ main.py
@@ -19,15 +19,21 @@ def result():
 
     ocha_data = zip(ocha.titles, ocha.descriptions, ocha.details)
     amerigeoos_data = zip(amerigeoos.titles, amerigeoos.descriptions, amerigeoos.details)
-    # if request.method == "POST":
-    #     # getting input_key in HTML form
-    #     input_key = request.form.get("search")
-    #     file_name = f"{path}/{input_key}.csv"
-    #     notice = f'You will also find the result in {path}'
-    #     download_data(input_key, file_name)
-    #     with open(f"{path}/{input_key}_by_prices.csv", encoding="UTF-8") as csv_file:
-    #         reader = csv.DictReader(csv_file)
-    #         return render_template('index.html', result = reader, notice=notice)
+    if request.method == "POST":
+        # getting input_key in HTML form
+        input_key = request.form.get("search")
+
+        ocha = Ocha(ocha_link + f"+{input_key}")
+        amerigeoos = Amerigeoos(amerigeoos_link + f"+{input_key}")
+
+        ocha_data = zip(ocha.titles, ocha.descriptions, ocha.details)
+        amerigeoos_data = zip(amerigeoos.titles, amerigeoos.descriptions, amerigeoos.details)
+
+        # notice = f'You will also find the result in {path}'
+        # download_data(input_key, file_name)
+        # with open(f"{path}/{input_key}_by_prices.csv", encoding="UTF-8") as csv_file:
+        #     reader = csv.DictReader(csv_file)
+        return render_template("index.html", ocha = ocha_data, amerigeoos = amerigeoos_data)
     return render_template("index.html", ocha = ocha_data, amerigeoos = amerigeoos_data)
 
 if __name__ == '__main__':
