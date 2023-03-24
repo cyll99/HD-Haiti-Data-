6f8f674f75242f690d296991f1cd802926984702
cyll99 <christyanlove@gmail.com>
2023-03-24 13:51:28 -0700

    include date in news

--- news.py
+++ news.py
@@ -38,9 +38,11 @@ class HaitiLibre():
             try:
                 image = "https://www.haitilibre.com" + container.find("img")["src"].strip()
                 title = container.find("img")["alt"]
+                date = container.findAll("td", {"class": "text"})[0].text.strip()
                 overview = container.findAll("td", {"class": "text"})[1].text.strip()
                 link = "https://www.haitilibre.com" + container.findAll("td", {"class": "text"})[2].a["href"]
-                date = container.findAll("td", {"class": "date"})[1].text.strip()
+                # date = container.findAll("span", {"class": "date"})[1].text.strip()
+                # print(date)
                 article = Article(image, title, overview, link, date)
                 self.articles.append(article)
             except:
@@ -67,7 +69,6 @@ class LeNouvelliste():
         # parses html into a soup data structure to traverse html
         # as if it were a json data type.
         soup = BeautifulSoup(webpage.content, "html.parser")
-        print(soup)
 
                 # finds news from home page
         containers = soup.findAll("div", {"class": "lnv-featured-article-sm"})
@@ -75,14 +76,15 @@ class LeNouvelliste():
         for container in containers:
             try:
                 image = container.find("img")["src"].strip()
-                title = container.find("h1").strip()
-                overview = container.findAll("div", {"class": "text-xs text-gray-500 mt-2"})[1].text.strip()
-                date = container.findAll("div", {"class": "text-gray-500 text-[16px] mt-4"})[1].text.strip()
+                title = container.find("img")["alt"].strip()
+                date = container.findAll("div", {"class": "text-xs text-gray-500 mt-2"})[0].text.strip()
+                overview = container.findAll("div", {"class": "text-gray-500 text-[16px] mt-4"})[0].text.strip()
                 link = "https://www.lenouvelliste.com" + container.find("a")["href"]
 
                 # print(f"image = {image}")
                 # print(f"title = {title}")
                 # print(f"overview = {overview}")
+                # print(f"overview = {date}")
                 # print(f"link = {link}")
 
 
@@ -128,4 +130,3 @@ class HaitiLoop():
             except:
                 continue
 
-# h = LeNouvelliste()
